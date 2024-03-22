6919a80006e5de4b65d8b3bafd52ad52c97c4088
Kevin Jiang <kevin@kevinjiang.dev>
2024-03-22 02:14:59 -0400

    Add debug log

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 FILENAME=JMusicBot-${JMUSICBOT_VERSION}.jar
 
 echo "Downloading ${FILENAME}..."
+echo ${JMUSICBOT_VERSION}
 wget https://github.com/jagrosh/MusicBot/releases/download/$JMUSICBOT_VERSION/JMusicBot-$JMUSICBOT_VERSION.jar
 
 echo "Starting JMusicBot"
